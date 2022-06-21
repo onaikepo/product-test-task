bd7f471ac2be70aa88f6a49d548604ded8865831
onaikepo <82336362+onaikepo@users.noreply.github.com>
2022-06-21 17:39:37 +0100

    updated folder structure

---
+++ Frontend/main.js
@@ -0,0 +1,14 @@
+
+function listAllProduct() {
+    //when the document.onReady it will need to fetch all of the SKU's from the database via PHP Script
+}
+
+function addProduct() {
+    //validate the fields are valid.
+    //then passes the input data to the php script via POST
+}
+
+function massDelete() {
+    //check which checkbox value evaluates to true and record those SKU's in an array. 
+    //POST the SKU's as a variable to the php script that need to be deleted. 
+}
